d86a478bd1fbe35d60d6c66a4561da3c3ff032e0
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2024-12-04 17:54:09 +0100

    Create `fetchWord` function to validate a word via external API

---
+++ src/utils/fetch-word.ts
@@ -0,0 +1,38 @@
+const API_URL: string = "https://words.dev-apis.com/validate-word";
+const WORD_TO_VALIDATE = "intent";
+
+interface ValidateWordResponse {
+  valid: boolean;
+  message: string;
+}
+
+export default async function fetchWord(url: string, word: string): Promise<ValidateWordResponse> {
+  try {
+    const response: Response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ word }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const processedResponse = await response.json();
+    return processedResponse;
+  } catch (error) {
+    console.error("An error occurred while fetching the word:", error);
+    throw error;
+  }
+}
+
+// async function main() {
+//   try {
+//     const result = await fetchWord(API_URL, WORD_TO_VALIDATE);
+//     console.log(result);
+//   } catch(error) {
+//     console.error(error);
+//   }
+// }
+
+// main();
