96b745f438f8e8526d2ab6d6bf6c218c114c9889
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2024-12-05 16:57:53 +0100

    Manage components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import { h } from "../../utils/dom";
+import { Header } from "../Header/Header";
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
+}
