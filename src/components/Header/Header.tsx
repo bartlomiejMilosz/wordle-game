822a6952f2edc1fe585898b93af0ca88092cee79
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2024-12-05 16:37:10 +0100

    Create simple Header component and manage styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import { h } from "../../utils/dom";
+import "./Header.css";
+
+export function Header() {
+
+  return (
+    <header className="header">
+      <h1 className="header-title">Welcome to My App</h1>
+      <nav className="header-nav">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
