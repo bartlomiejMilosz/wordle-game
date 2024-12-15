850c455846f54e60e452807f159876e005988e88
Bartłomiej Miłosz <bartlomiej.milosz@gmail.com>
2024-12-15 11:10:41 +0100

    Create 'backspace' function for removing last letter from grid

--- src/handlers/handle-backspace.ts
+++ src/handlers/handle-backspace.ts
@@ -1,3 +1,19 @@
-export function handleBackspace() {
-  
+import { gameState } from "../app-state";
+
+function handleBackspace(gridItems: NodeListOf<HTMLDivElement>) {
+	if (gameState.currentGuess.length === 0) {
+		console.error("Cannot backspace an empty guess.");
+		return;
+	}
+
+	const currentIndex: number = gameState.currentGuess.length - 1;
+	if (currentIndex >= gridItems.length) {
+		console.error(`Index ${currentIndex} is out of bounds for grid items.`);
+		return;
+	}
+
+	gameState.currentGuess = gameState.currentGuess.slice(0, -1);
+	gridItems[currentIndex].innerText = "";
 }
+
+export { handleBackspace };
